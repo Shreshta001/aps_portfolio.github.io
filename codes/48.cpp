374d5ce35523e91eb43de03eda60e5369320c79c
Shreshta S. Majalikar <yesyesem22@gmail.com>
2025-06-07 17:00:38 +0530

    Update 48.cpp

--- codes/48.cpp
+++ codes/48.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class LogTable {
+private:
+    vector<int> log;
+
+public:
+    LogTable(int n) {
+        log.resize(n + 1);
+        log[1] = 0;
+        for (int i = 2; i <= n; i++)
+            log[i] = log[i / 2] + 1;
+    }
+
+    int getLog(int x) const {
+        return log[x];
+    }
+
+    int operator[](int x) const {
+        return log[x];
+    }
+};
 
